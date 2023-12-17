9f2c479e42105cc570b9a47cf09a08f10bee3eb6
Abdallah Magdy <bodyalsadek2002@gmail.com>
2023-12-17 23:14:25 +0200

    Add fake testimonials

--- src/app/(landing)/_components/LandingContent.tsx
+++ src/app/(landing)/_components/LandingContent.tsx
@@ -9,40 +9,46 @@ import {
 
 const testimonials = [
   {
-    name: 'Abdallah',
-    avatar: 'A',
-    title: 'Software Engineer',
-    description: "This is the best application I've used!",
+    name: 'Emily Thompson',
+    avatar: 'E',
+    title: 'Graphic Designer',
+    description:
+      "Magus is a game-changer for my creative process! The AI-powered image generation is mind-blowing, and the chatbot is incredibly intelligent. It's become an indispensable tool in my workflow.",
   },
   {
-    name: 'Abdallah',
+    name: 'Alex Ramirez',
     avatar: 'A',
-    title: 'Software Engineer',
-    description: "This is the best application I've used!",
+    title: 'Data Scientist',
+    description:
+      "As a data scientist, Magus has exceeded my expectations. The code generation capabilities are impressive, and the AI assistant provides valuable insights. It's like having a brilliant coding partner at my fingertips.",
   },
   {
-    name: 'Abdallah',
-    avatar: 'A',
-    title: 'Software Engineer',
-    description: "This is the best application I've used!",
+    name: 'Sophia Chen',
+    avatar: 'S',
+    title: 'Music Producer',
+    description:
+      "Magus has revolutionized my music production process. The music generation feature is outstanding, helping me explore new melodies and harmonies effortlessly. It's a must-have for any musician!",
   },
   {
-    name: 'Abdallah',
-    avatar: 'A',
-    title: 'Software Engineer',
-    description: "This is the best application I've used!",
+    name: 'Carlos Rodriguez',
+    avatar: 'C',
+    title: 'Video Editor',
+    description:
+      "I can't imagine my video editing without Magus. The AI-driven video generation saves me so much time, and the results are stunning. It's like having a virtual assistant that understands my creative vision.",
   },
   {
-    name: 'Abdallah',
-    avatar: 'A',
-    title: 'Software Engineer',
-    description: "This is the best application I've used!",
+    name: 'Linda Johnson',
+    avatar: 'L',
+    title: 'Web Developer',
+    description:
+      "Magus is a developer's dream come true! The code generation feature is a lifesaver, and the intelligent assistant makes problem-solving a breeze. It has become an essential part of my toolkit.",
   },
   {
-    name: 'Abdallah',
-    avatar: 'A',
-    title: 'Software Engineer',
-    description: "This is the best application I've used!",
+    name: 'David Smith',
+    avatar: 'D',
+    title: 'Content Creator',
+    description:
+      "Magus has transformed the way I create content. The AI magic behind image, video, and code generation is unparalleled. It's like having a versatile creative partner that never runs out of ideas!",
   },
 ];
 
